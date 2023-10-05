e782e63a24bd5d685f0a6fe704f26ca81c15389c
sneakybeakygit <cjmgames@gmail.com>
2023-10-04 23:17:28 -0400

    sound changes

--- clippybot.py
+++ clippybot.py
@@ -1,6 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 from nextcord.ext import tasks
+from nextcord.ui import Button, View
 import time
 import shelve
 from random import randrange
@@ -11,9 +12,6 @@ import subprocess
 import json
 
 # TO-DO
-#   either make miners "computers" or just make them seb slaves
-#   store how many miners a player has, calculate cost for them when they run the shop command
-#   cost of miner => price = basecost * 1.12^(num_owned)
 #   shop buttons to show individual prices privately
 
 
@@ -22,6 +20,14 @@ import json
 #   itemvault = {
 #               'vaultid1' : [quantity, quantity, ... ]}
 #   list index represents which item it is (numbers match up with the shop number - 1)
+
+# BEBBIES POTENTIAL FEATURES
+# $mine scales with income
+# $rtd command :
+#   gives random effects such as 2x mining speed, 0.5x mining speed, etc. (more ideas needed)
+
+# RANDOM IDEAS
+# trading cards
 
 
 # variables for the $mine command
@@ -79,16 +85,28 @@ minerIDs = {0:'Sneaky Slave',
             9:'Bebastian Plantation',
             10:'Joe Miner'}
 
-saved_sounds = []
 
 intents = nextcord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-client = commands.Bot(command_prefix = '$', intents = intents)
+#client = commands.Bot(command_prefix = '$', intents = intents)
+class CustomBot(commands.Bot): # instead of using global variables, create variables in bot class
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.saved_sounds = None
+        self.stop_sound = False
+        self.old_vc = None
+        self.last_channel = None
 
-# ---- RANDOM COMMANDS ---------------------------------------------------
+client = CustomBot(command_prefix="$", intents=intents)
 
+# load extensions
+client.load_extension("sound_commands")
+
+# ------------------------------------------------------------------------ #
+#                            RANDOM COMMANDS                               #
+# ------------------------------------------------------------------------ #
 #@client.command()
 #async def info(ctx):
 #    await ctx.send("Info\n$clippy - Talk to clippy\n$beb - Let beb know you need his attention\n$richest - See the richest bebbies in the server\n$vault - See what's in the vault\n$mine - Mine some bebbies\n$balance - See how many bebbies you have")
@@ -107,69 +125,58 @@ async def clippy(ctx):
 
 @client.command()
 async def play(ctx, url: str):
-    subprocess.run([os.getcwd() + "/yt-dlp", "-x", "--audio-format", "mp3", url, "-o", "yt.mp3"])
-    mp3_exists = os.path.exists("yt.mp3")
-    if mp3_exists:
-        vc = await ctx.author.voice.channel.connect()
-        vc.play(nextcord.FFmpegPCMAudio(source = "yt.mp3"))
-        while vc.is_playing():
-            await asyncio.sleep(.25)
-        vc.stop()
-        await vc.disconnect()
-        os.remove("yt.mp3")
-
-
-
-# ------------------------------------------------------------------------
-#                            SOUND EFFECTS
-# ------------------------------------------------------------------------
+    if ctx.channel.id == 884995892359331850: # bot spam text channel
+        client.last_channel
+        client.stop_sound = False
+        subprocess.run([os.getcwd() + "/yt-dlp", "-x", "--audio-format", "mp3", url, "-o", "yt.mp3"])
+        mp3_exists = os.path.exists("yt.mp3")
+        if mp3_exists:
+            vc = None
+            if ctx.author.voice.channel.id != client.last_channel or client.old_vc == None:
+                #await client.get_guild(402256672028098580).change_voice_state(None)
+                #vc = nextcord.utils.get(client.voice_clients, guild = ctx.guild)
+                if client.old_vc != None:
+                    await client.old_vc.disconnect()
+                vc = await ctx.author.voice.channel.connect()
+                client.last_channel = ctx.author.voice.channel.id
+                client.old_vc = vc
+                await client.get_guild(402256672028098580).change_voice_state(channel = ctx.author.voice.channel, self_deaf = True) # server id
+            else:
+                vc = client.old_vc
+            vc.play(nextcord.FFmpegPCMAudio(source = "yt.mp3"))
+            while vc.is_playing() and not client.stop_sound:
+                await asyncio.sleep(.25)
+            vc.stop()
+            #await vc.disconnect()
+            os.remove("yt.mp3")
 
 @client.command()
-async def sounds(ctx, sound: str):
-    file_name = "sounds/"
-    flag = True
-    match sound:
-        case "amogus":
-            file_name += "amogus.m4a"
-        case "augh":
-            file_name += "augh.mp3"
-        case "pbnj":
-            file_name += "pbnj.mp3"
-        case "ayoterry":
-            file_name += "ayoterry.mp3"
-        case "bababooey":
-            file_name += "bababooey.3gp"
-        case "dog":
-            file_name += "dog.3gp"
-        case "error":
-            file_name += "error.3gp"
-        case "marioo":
-            file_name += "marioo.3gp"
-        case "taco":
-            file_name += "taco.3gp"
-        case "usb":
-            file_name += "usb.3gp"
-        case "wenkwenk":
-            file_name += "wenkwenk.mp3"
-        case "aight":
-            file_name += "aight.mp3"
-        case _:
-            found_file = False
-            for sounds in saved_sounds:
-                if sound == sounds[0]:
-                    file_name += "saved_sounds/" + sounds[0] + sounds[1]
-                    found_file = True
-                    break
-            if not found_file:
-                await ctx.send(f"{sound} is not recognized. Type $sounds to see the options.")
-                flag = False
-    if flag:
-        vc = await ctx.author.voice.channel.connect()
-        vc.play(nextcord.FFmpegPCMAudio(source = file_name))
-        while vc.is_playing():
-            await asyncio.sleep(.25)
-        vc.stop()
-        await vc.disconnect()
+async def stop(ctx):
+    if ctx.channel.id == 884995892359331850:
+        client.stop_sound = True
+        await asyncio.sleep(.1)
+
+@client.command()
+async def summon(ctx):
+    if ctx.channel.id == 884995892359331850:
+
+        if client.last_channel != None and ctx.author.voice.channel.id != client.last_channel:
+            #await client.get_guild(402256672028098580).change_voice_state(channel = None)
+            client.last_channel = ctx.author.voice.channel.id
+            await client.old_vc.disconnect()
+            client.old_vc = await ctx.author.voice.channel.connect()
+            await client.get_guild(402256672028098580).change_voice_state(channel = ctx.author.voice.channel, self_deaf = True)
+        elif client.last_channel == None:
+            client.last_channel = ctx.author.voice.channel.id
+            client.old_vc = await ctx.author.voice.channel.connect()
+            await client.get_guild(402256672028098580).change_voice_state(channel = ctx.author.voice.channel, self_deaf = True)
+
+# ------------------------------------------------------------------------ #
+#                            SOUND EFFECTS                                 #
+# ------------------------------------------------------------------------ #
+
+@client.command()
+
 
 # ------------------------------------------------------------------------
 #                   RESET ACTIVITY STATUS OF MEMBERS
@@ -207,7 +214,6 @@ async def richest(ctx):
             await ctx.send(embed = richestEmbed)
         else:
             await ctx.send("You're all broke! Ya mommas broke, ya daddys broke, ya brother broke, ya sister, ya cousins broke, ya auntie broke, seb is broke.")
-
 
 @client.command()
 async def vault(ctx):
@@ -425,11 +431,9 @@ async def register(ctx, username: str):
                 vault[userID] = newPlayer
                 await ctx.send(f'You have been registered as {username}')
 
-
 # ------------------------------------------------------------------------
 #
 # ---- ADMIN COMMANDS ----------------------------------------------------
-
 
 @client.command()
 async def delete(ctx, sound: str):
@@ -441,10 +445,10 @@ async def delete(ctx, sound: str):
         if sound in sound_dict:
             extension = ""
             del sound_dict[sound]
-            for i in range(len(saved_sounds)):
-                if saved_sounds[i][0] == sound:
-                    extension = saved_sounds[i][1]
-                    del saved_sounds[i]
+            for i in range(len(client.saved_sounds)):
+                if client.saved_sounds[i][0] == sound:
+                    extension = client.saved_sounds[i][1]
+                    del client.saved_sounds[i]
                     break
 
             txt = open("added_sounds.json", "w")
@@ -476,26 +480,25 @@ async def name(ctx, userID, username: str):
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-    try:
-        txt = open("added_sounds.json", "r")#read filenames
-
-        global saved_sounds
-        saved_sounds = []
-        
-        sound_dict = json.load(txt)
-        
-        for sound in sound_dict:
-            extension = '.' + sound_dict[sound]["type"]
-            saved_sounds.append([sound, extension])#add filename to list
-
-        txt.close()
-    except FileNotFoundError:
-        pass
-
 @client.event
 async def on_voice_state_update(member, before, after):
+
+    if member.id == client.user.id and after.channel == None:
+        client.old_vc = None
+        client.last_channel = None
+        return
+
     if not before.channel and after.channel and after.channel.id != 402257227555143701 and not member.id == client.user.id and not member.id == 159800228088774656:
-        vc = await after.channel.connect()
+        vc = client.old_vc
+
+        if after.channel.id != client.last_channel or client.old_vc == None:
+            if client.old_vc != None:
+                await client.old_vc.disconnect()
+            vc = await after.channel.connect()
+            client.last_channel = after.channel.id
+            client.old_vc = vc
+            await client.get_guild(402256672028098580).change_voice_state(channel = after.channel, self_deaf = True)
+
         #textchannel = client.get_channel(884995892359331850) #bot spam
         await asyncio.sleep(.25)
         randval = randrange(0, 20)
@@ -515,7 +518,15 @@ async def on_voice_state_update(member, before, after):
         await vc.disconnect()
 
     elif before.channel and before.channel.id == 402257227555143701 and after.channel and after.channel.id != 402257227555143701 and not member.id == client.user.id and not member.id == 159800228088774656:
-        vc = await after.channel.connect()
+        vc = client.old_vc
+
+        if after.channel.id != client.last_channel or client.old_vc == None:
+            if client.old_vc != None:
+                await client.old_vc.disconnect()
+            vc = await after.channel.connect()
+            client.last_channel = after.channel.id
+            client.old_vc = vc
+            await client.get_guild(402256672028098580).change_voice_state(channel = after.channel, self_deaf = True)
         #textchannel = client.get_channel(884995892359331850) #bot spam
         await asyncio.sleep(.25)
         randval = randrange(0, 20)
@@ -535,62 +546,22 @@ async def on_voice_state_update(member, before, after):
         await vc.disconnect()
 
     elif before.channel and before.channel.id == 929075584020127834 and after.channel and not member.id == client.user.id:
-        vc = await after.channel.connect()
+        vc = client.old_vc
+
+        if after.channel.id != client.last_channel or client.old_vc == None:
+            if client.old_vc != None:
+                await client.old_vc.disconnect()
+            vc = await after.channel.connect()
+            client.last_channel = after.channel.id
+            client.old_vc = vc
+            await client.get_guild(402256672028098580).change_voice_state(channel = after.channel, self_deaf = True)
+
         await asyncio.sleep(.25)
         vc.play(nextcord.FFmpegPCMAudio(source = "sounds/hagay.mp3"))
         while vc.is_playing():
             await asyncio.sleep(.25)
         vc.stop()
-        await vc.disconnect()
-
-
-
-@client.event
-async def on_message(msg):
-    author = msg.author
-
-    if author == client.user:
-        return
-
-    dm = msg.channel.type == nextcord.ChannelType.private
-
-    if not dm:
-        await client.process_commands(msg)
-        return
-
-    guild = client.get_guild(402256672028098580)#the boys
-    #guild = client.get_guild(968624369683275856) # clippy test server
-    member = guild.get_member(author.id)
-
-    if member:
-        for r in member.roles:
-            if r.id == 501542465623556116:#beaky id     #1139361424217485382: reaction role id for test server
-                if msg.content == "" and msg.attachments and msg.attachments[0].size < 10000000:
-                    file_name = msg.attachments[0].filename.lower()
-                    for sound in saved_sounds:
-                        if sound[0] + sound[1] == file_name:
-                            await msg.reply(file_name + " is already taken. Change the name and try again.")
-                            return
-
-                    txt = open("added_sounds.json", "r")#add filename to file
-                    sound_dict = json.load(txt)
-                    txt.close()
-                    
-                    filetype = file_name[file_name.index('.')+1:]
-                    sound_name = file_name[:file_name.index('.')]
-                    
-                    sound_dict[sound_name] = {"type":filetype, "author":author.id}
-                    
-                    txt = open("added_sounds.json", "w")
-                    json.dump(sound_dict, txt, indent = 4)
-                    txt.close()
-                    
-                    extension = file_name[file_name.index('.'):]
-                    saved_sounds.append([file_name[:file_name.index('.')], extension])#add filename to list
-
-                    await msg.attachments[0].save(os.getcwd() + "/sounds/saved_sounds/" + msg.attachments[0].filename.lower())
-                    await msg.reply(file_name + " successfully added!")
-                return
+        #await vc.disconnect()
 
 @client.event
 async def on_reaction_add(reaction, user):
@@ -607,6 +578,7 @@ async def on_reaction_add(reaction, user):
                 print('3')
                 await user.add_roles(role)
                 await reaction.message.author.send(f'You have been given the role "{role.name}".')
+
 @client.event
 async def on_reaction_remove(reaction, user):
     if reaction.message.channel.id == welcome_channel_id:
@@ -615,8 +587,6 @@ async def on_reaction_remove(reaction, user):
             if role:
                 await user.remove_roles(role)
                 channel = client.get_channel(welcome_channel_id)
-
-
 
 # ------------------------------------------------------------------------
 #
@@ -629,7 +599,6 @@ async def miner_income():
             tempPlayer = vault[userID]
             tempPlayer.add_balance(float(tempPlayer.get_income() * 5))
             vault[userID] = tempPlayer
-
 
 miner_income.start()
 
@@ -657,42 +626,7 @@ async def name_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Usage: $name [userID] [username]\nuserID - discord member ID\nusername must not contain spaces or special characters')
 
-@sounds.error
-async def sounds_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        sounds_page_number = 1 #current number of the embed being created
-        sounds_added_count = 0 #how many sounds are on a given embed
-        soundsEmbed = nextcord.Embed(title = f'Sounds Page {sounds_page_number}', color = embedBlue) #sets up first embed
 
-        #adds original sounds and updates sounds_added_count
-        soundsEmbed.add_field(name = 'amogus', value = '')
-        soundsEmbed.add_field(name = 'augh', value = '')
-        soundsEmbed.add_field(name = 'pbnj', value = '')
-        soundsEmbed.add_field(name = 'ayoterry', value = '')
-        soundsEmbed.add_field(name = 'bababooey', value = '')
-        soundsEmbed.add_field(name = 'dog', value = '')
-        soundsEmbed.add_field(name = 'error', value = '')
-        soundsEmbed.add_field(name = 'marioo', value = '')
-        soundsEmbed.add_field(name = 'taco', value = '')
-        soundsEmbed.add_field(name = 'usb', value = '')
-        soundsEmbed.add_field(name = 'wenkwenk', value = '')
-        soundsEmbed.add_field(name = 'aight', value = '')
-        sounds_added_count += 12
-
-        #iterates through all sounds in the saved sounds file
-        for s in saved_sounds:
-            soundsEmbed.add_field(name = s[0], value = '') #adds sound to embed
-            sounds_added_count += 1 #update sound count
-            if sounds_added_count == 25: #once 25 sounds are added, print the embed and setup the next embed
-                await ctx.send(embed=soundsEmbed)
-                sounds_page_number += 1
-                soundsEmbed = nextcord.Embed(title = f'Sounds Page {sounds_page_number}', color = embedBlue)
-                sounds_added_count = 0
-
-        # at this point there are no more sounds in the saved_sounds file to add
-        # if the current embed has any sounds on it, print (used to prevent an empty embed from being printed because of a sound count that is a multiple of 25)
-        if sounds_added_count > 0:
-            await ctx.send(embed=soundsEmbed)
 
 # ------------------------------------------------------------------------
 
